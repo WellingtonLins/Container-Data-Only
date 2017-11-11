b412c640dc04c5d7f85c32824c5d9853b047c71b
WellingtonLins <wellingtonlins2013@gmail.com>
2017-11-11 15:13:05 -0300

    Trabalhando com Compposer, Volume e Network no docker

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-mvn clean package
-docker-compose up -d
+# docker network create antenas-de-vinil
 # docker build -t ricardojob/banco ./postgres
 # docker run -p 5433:5432 -d --name banco -v $(pwd)/data:/var/lib/postgresql/data ricardojob/banco
 # mvn clean package
@@ -7,3 +6,6 @@ docker-compose up -d
 # docker run -p 8082:8080 -d --name app --link banco:host-banco ricardojob/app
 # x-www-browser localhost:8082/app
 
+ docker network create antenas-de-vinil
+ mvn clean package
+ docker-compose up -d
--- stop.sh
+++ stop.sh
@@ -1,11 +1,14 @@
-docker stop app
-docker kill app
-docker rm app
-docker rmi -f ricardojob/app
+# docker stop app
+# docker kill app
+# docker rm app
+# docker rmi -f ricardojob/app
 
-docker stop banco
-docker kill banco
-docker rm banco
-docker rmi -f ricardojob/banco
-# docker-compose down
-# mvn clean
+# docker stop banco
+# docker kill banco
+# docker rm banco
+# docker rmi -f ricardojob/banco
+# docker network rm antenas-de-vinil
+
+docker-compose down
+docker network rm antenas-de-vinil
+mvn clean
