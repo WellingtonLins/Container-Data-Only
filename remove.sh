7023a02c87d645ef39a0c682441bbfb79d4067ed
Wellingtonlins <wellingtonlins2013@gmail.com>
2018-03-05 21:47:11 -0300

    Adicionando arquivo remove.sh

--- remove.sh
+++ remove.sh
@@ -1,3 +1,8 @@
-
+ docker stop app
+ docker stop banco
+ docker rm app
+ docker rm banco
  docker rmi rede-docker crud-livro/app
  docker rmi rede-docker crud-livro/banco
+ docker network  rm  rede-docker
+
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-# docker network create rede-docker
-# docker build -t ricardojob/banco ./postgres
-# docker run -p 5433:5432 -d --name banco -v $(pwd)/data:/var/lib/postgresql/data ricardojob/banco
-# mvn clean package
-# docker build -t ricardojob/app .
-# docker run -p 8082:8080 -d --name app --link banco:host-banco ricardojob/app
+ docker network create rede-docker
+ docker build -t ricardojob/banco ./postgres
+ docker run -p 5433:5432 -d --name banco -v $(pwd)/data:/var/lib/postgresql/data ricardojob/banco
+ mvn clean package
+ docker build -t ricardojob/app .
+ docker run -p 8082:8080 -d --name app --link banco:host-banco ricardojob/app
 # x-www-browser localhost:8082/app
 
- docker network create rede-docker
- mvn clean package
- docker-compose up -d
+#docker network create rede-docker
+#mvn clean package
+#docker-compose up -d
